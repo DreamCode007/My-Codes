c61ff38a18e43ecf55481be315fe7626647ec85a
Junaid Mustafa <mustafajunaid3000@gmail.com>
2025-10-24 13:46:27 +0530

    Fix comment typo in TowerOfHanoi function

--- TowerOfHanoi.c
+++ TowerOfHanoi.c
@@ -4,7 +4,7 @@ void tow(int a, char s, char h, char d)
 {
     
     if (a == 0 ) return;
-    tow(a-1, s, d, h); // s is source ,h is hel prod , d is destination 
+    tow(a-1, s, d, h); // s is source ,h is helping rod , d is destination 
     printf("%c to %c\n", s, d);
     
     
